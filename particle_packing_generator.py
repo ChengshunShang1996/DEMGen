9aa1ad627046ef8db5fbd1affae5a1dabd3ff707
Chengshun Shang <chengshun.shang1996@gmail.com>
2023-03-23 20:12:21 +0100

    add stop time seeting

--- particle_packing_generator.py
+++ particle_packing_generator.py
@@ -15,13 +15,17 @@ class ParticlePackingGenerator(DEMAnalysisStage):
 
     def Initialize(self):
         super().Initialize()
-        self.InitializePackingGenerator()  
+        self.InitializePackingGenerator() 
+        self.GetInitialDemSphereVolume()
+        self.dt = self.spheres_model_part.ProcessInfo[DELTA_TIME]
 
     def InitializePackingGenerator(self):
 
         #define the aim porosity
         self.aim_final_packing_porosity = 0.35
         self.aim_container_filling_ratio = 0.8 #this means the inlet will stop when the generated particel's volume occupies [aim_container_filling_ratio * container_volume]
+        self.container_filling_ratio = 0.0
+        self.initial_sphere_volume = 0.0
         
         self.container_shape = "cylinder"  #input: "cylinder" or "box"  
 
@@ -66,7 +70,36 @@ class ParticlePackingGenerator(DEMAnalysisStage):
     def KeepAdvancingSolutionLoop(self):
         
         return self.time < self.end_time
+    
+    def FinalizeSolutionStep(self):
+        super().FinalizeSolutionStep()
+        self.CalculateFillingRatioAndSetInletStop()
 
+    def GetInitialDemSphereVolume(self):
+        
+        for element in self.spheres_model_part.Elements:
+            r = element.GetNode(0).GetSolutionStepValue(RADIUS)
+            element_volume = 4/3 * math.pi * r * r * r
+            self.initial_sphere_volume += element_volume
+
+    def CalculateFillingRatioAndSetInletStop(self):
+        
+        #calculte container filling ratio
+        total_element_volume = 0.0
+        for element in self.spheres_model_part.Elements:
+            r = element.GetNode(0).GetSolutionStepValue(RADIUS)
+            element_volume = 4/3 * math.pi * r * r * r
+            total_element_volume += element_volume
+
+        generated_element_volume = total_element_volume - self.initial_sphere_volume
+        self.container_filling_ratio = generated_element_volume / self.container_volume
+
+        #set Inlet stop time
+        if self.container_filling_ratio > self.aim_container_filling_ratio:
+
+            for submp in self.dem_inlet_model_part.SubModelParts:
+                if submp.Has(INLET_STOP_TIME):
+                    submp[INLET_STOP_TIME] = self.time + self.dt
 
 if __name__ == "__main__":
 
