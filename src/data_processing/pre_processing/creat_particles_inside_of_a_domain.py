f90a8ee19c7477a5dba9abb7886984a18fc1dc9c
Chengshun Shang <chengshun.shang1996@gmail.com>
2024-10-03 11:24:33 +0200

    minor

--- src/data_processing/pre_processing/creat_particles_inside_of_a_domain.py
+++ src/data_processing/pre_processing/creat_particles_inside_of_a_domain.py
@@ -112,15 +112,12 @@ class CreatParticlesInsideOfADomain():
         shutil.copyfile(seed_file_path_and_name, aim_file_path_and_name)
         
     def inverse_cdf(self, u, x_max):
-        # 对 f(x) = 1 / (x + 1)^3，累积分布函数 (CDF) 是 F(x) = 1 - 1 / (x + 1)^2
-        # 在区间 [0, x_max] 上，累积值 F(x_max) = 1 - 1 / (x_max + 1)^2
+
         F_max = 1 - 1 / (x_max + 1)**2
 
-        # 归一化均匀分布随机数 u 到 [0, F_max]
         u_scaled = u * F_max
 
-        # 通过逆CDF公式生成随机数: x = (1 / sqrt(1 - u)) - 1
-        return (1 / np.sqrt(1 - u_scaled)) - 1
+        return (1 / math.sqrt(1 - u_scaled)) - 1
 
     def CreatParticles(self, RVE_size):
 
