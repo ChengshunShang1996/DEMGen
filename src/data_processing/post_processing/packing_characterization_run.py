2c41b3833b0a666f6e2c4d92d319a57e208aefef
jgonzalezusua <jgonzalez@cimne.upc.edu>
2025-03-10 15:50:22 +0100

    including conductivity

--- src/data_processing/post_processing/packing_characterization_run.py
+++ src/data_processing/post_processing/packing_characterization_run.py
@@ -26,9 +26,10 @@ class ParticlePackingCharacterizationRun(DEMAnalysisStage):
         with open('ParametersDEMGen.json', 'r') as file:
             self.parameters_DEMGen = json.load(file)
 
+
     def Initialize(self):
         super().Initialize()
-        self.InitializePackingCharacterization() 
+        self.InitializePackingCharacterization()
         #self.GetInitialDemSphereVolume()
 
     def InitializePackingCharacterization(self):
@@ -57,9 +58,9 @@ class ParticlePackingCharacterizationRun(DEMAnalysisStage):
             self.OutputSolutionStep()
 
     def KeepAdvancingSolutionLoop(self):
-        
+
         return self.time < self.end_time
-    
+
     def FinalizeSolutionStep(self):
         super().FinalizeSolutionStep()
         time_step = self.spheres_model_part.ProcessInfo[TIME_STEPS]
@@ -68,7 +69,7 @@ class ParticlePackingCharacterizationRun(DEMAnalysisStage):
             self.PlotAndSaveResultsInPDF()
 
     def MeasureLocalPropertiesWithDifferentRadius(self):
-        
+
         print("Start measurement")
         max_diameter = self.parameters_DEMGen["particle_radius_max"] * 2.0
         RVE_lambda = self.parameters_DEMGen["packing_charcterization_setting"]["RVE_lambda_initial"]
@@ -84,25 +85,30 @@ class ParticlePackingCharacterizationRun(DEMAnalysisStage):
         measured_mean_coordination_number = []
         measured_eigenvalues = []
         measured_second_invariant_of_deviatoric_tensor = []
+        measured_conductivity = []
 
         while side_length <= side_length_max:
 
             RVE_lambda_list.append(RVE_lambda)
             if self.parameters_DEMGen["packing_charcterization_setting"]["measure_density_option"]:
                 measured_packing_density.append(1 - self.MeasureSphereForGettingPackingProperties((side_length/2), center_x, center_y, center_z, 'porosity'))
-            
+
             if self.parameters_DEMGen["packing_charcterization_setting"]["measure_mean_coordination_number_option"]:
                 measured_mean_coordination_number.append(self.MeasureSphereForGettingPackingProperties((side_length/2), center_x, center_y, center_z, 'averaged_coordination_number'))
-            
+
             if self.parameters_DEMGen["packing_charcterization_setting"]["measure_anisotropy_option"]:
                 eigenvalues, second_invariant_of_deviatoric_tensor, measured_fabric_tensor = self.MeasureSphereForGettingPackingProperties((side_length/2), center_x, center_y, center_z, 'fabric_tensor')
                 measured_eigenvalues.append(eigenvalues)
                 measured_second_invariant_of_deviatoric_tensor.append(second_invariant_of_deviatoric_tensor)
-            
+
+            if self.parameters_DEMGen["packing_charcterization_setting"]["measure_conductivity_tensor_option"]:
+                eigenvalues, second_invariant_of_deviatoric_tensor, measured_non_homogenized_conductivity_tensor = self.MeasureSphereForGettingPackingProperties((side_length/2), center_x, center_y, center_z, 'conductivity_tensor')
+                measured_conductivity.append(measured_non_homogenized_conductivity_tensor)
+
             '''
             if self.parameters_DEMGen["packing_charcterization_setting"]["measure_radia_distribution_function_option"]:
                 self.MeasureSphereForGettingRadialDistributionFunction(side_length/2, center_x, center_y, center_z, max_diameter/15, max_diameter)
-            
+
             if self.parameters_DEMGen["packing_charcterization_setting"]["measure_voronoi_input_option"]:
                 self.MeasureCubicForGettingPackingProperties(side_length, center_x, center_y, center_z, 'voronoi_input_data')
             '''
@@ -110,22 +116,27 @@ class ParticlePackingCharacterizationRun(DEMAnalysisStage):
             RVE_lambda += self.parameters_DEMGen["packing_charcterization_setting"]["RVE_lambda_increment"]
             side_length = max_diameter * RVE_lambda
             #side_length = round(side_length, 4)
-        
+
         if self.parameters_DEMGen["packing_charcterization_setting"]["measure_density_option"]:
             with open("packing_properties_density.txt", "w") as f_w:
                 for i in range(len(RVE_lambda_list)):
                     f_w.write(str(RVE_lambda_list[i]) + ' '+ str(measured_packing_density[i]) + '\n')
-                    
+
         if self.parameters_DEMGen["packing_charcterization_setting"]["measure_mean_coordination_number_option"]:
             with open("packing_properties_mean_coordination_number.txt", "w") as f_w:
                 for i in range(len(RVE_lambda_list)):
                     f_w.write(str(RVE_lambda_list[i]) + ' ' + str(measured_mean_coordination_number[i]) + '\n')
-                    
+
         if self.parameters_DEMGen["packing_charcterization_setting"]["measure_anisotropy_option"]:
             with open("packing_properties_anisotropy.txt", "w") as f_w:
                 for i in range(len(RVE_lambda_list)):
                     f_w.write(str(RVE_lambda_list[i]) + ' ' + str(measured_eigenvalues[i][0]) + ' '+ str(measured_eigenvalues[i][1]) + ' '+\
                                str(measured_eigenvalues[i][2]) + ' ' + str(measured_second_invariant_of_deviatoric_tensor[i]) + '\n')
+
+        if self.parameters_DEMGen["packing_charcterization_setting"]["measure_conductivity_tensor_option"]:
+            with open("packing_properties_conductivity.txt", "w") as f_w:
+                for i in range(len(RVE_lambda_list)):
+                    f_w.write(str(RVE_lambda_list[i]) + ' '+ str(measured_conductivity[i]) + '\n')
 
         print("Measurement finish")
 
@@ -136,11 +147,13 @@ class ParticlePackingCharacterizationRun(DEMAnalysisStage):
 
         file_names = []
         if self.parameters_DEMGen["packing_charcterization_setting"]["measure_density_option"]:
-            file_names.append("packing_properties_density.txt")     
+            file_names.append("packing_properties_density.txt")
         if self.parameters_DEMGen["packing_charcterization_setting"]["measure_mean_coordination_number_option"]:
             file_names.append("packing_properties_mean_coordination_number.txt")
         if self.parameters_DEMGen["packing_charcterization_setting"]["measure_anisotropy_option"]:
             file_names.append("packing_properties_anisotropy.txt")
+        if self.parameters_DEMGen["packing_charcterization_setting"]["measure_conductivity_tensor_option"]:
+            file_names.append("packing_properties_conductivity.txt")
 
         github_link = "https://github.com/ChengshunShang1996/DEMGen"
         current_time = datetime.now().strftime('%Y-%m-%d')
@@ -154,63 +167,65 @@ class ParticlePackingCharacterizationRun(DEMAnalysisStage):
             plt.text(0.5, 0.3, github_link, fontsize=8, ha='center', color='blue')
 
             if len(file_names) == 0:
-                plt.text(0.5, 0.1, "NO RESULTS IS PRESENTED!!!", fontsize=15, ha='center', color='red')
-            
+                plt.text(0.5, 0.1, "NO RESULTS ARE PRESENTED!!!", fontsize=15, ha='center', color='red')
+
             # Hide axes for the cover page
             plt.axis('off')
-            
+
             # Save the cover page to the PDF
             pdf.savefig()
             plt.close()
 
             for file_name in file_names:
-                
+
                 if file_name == "packing_properties_density.txt":
                     lambda_list = []
                     density_list = []
-                    
+
                     with open(file_name, 'r') as file:
                         for line in file:
                             my_lambda, density = map(float, line.split())
                             lambda_list.append(my_lambda)
                             density_list.append(density)
-                    
+
                     plt.figure(figsize=(8, 6))
                     plt.plot(lambda_list, density_list, marker='o')
-                    
+
                     plt.xlabel('$\lambda$')
+
                     plt.ylabel('Packing density')
                     plt.title(f'Measured packing density')
                     #plt.legend()
                     plt.grid('both')
-                    
+
                     pdf.savefig()
                     plt.close()
-                
+
                 elif file_name == "packing_properties_mean_coordination_number.txt":
                     lambda_list = []
                     mcn_list = []
-                    
+
                     with open(file_name, 'r') as file:
                         for line in file:
                             my_lambda, mcn = map(float, line.split())
                             lambda_list.append(my_lambda)
                             mcn_list.append(mcn)
-                    
+
                     plt.figure(figsize=(8, 6))
                     plt.plot(lambda_list, mcn_list, marker='o')
-                    
+
                     plt.xlabel('$\lambda$')
+
                     plt.ylabel('MCN')
                     plt.title(f'Measured mean coordination number (MCN)')
                     #plt.legend()
                     plt.grid('both')
-                    
+
                     pdf.savefig()
                     plt.close()
 
                 elif file_name == "packing_properties_anisotropy.txt":
-                    lambda_list = []
+                    self.lambda_list = []
                     eigenvalue_1_list = []
                     eigenvalue_2_list = []
                     eigenvalue_3_list = []
@@ -219,37 +234,61 @@ class ParticlePackingCharacterizationRun(DEMAnalysisStage):
                     with open(file_name, 'r') as file:
                         for line in file:
                             my_lambda, eigenvalue_1, eigenvalue_2, eigenvalue_3, anisotrpy_intensity = map(float, line.split())
-                            lambda_list.append(my_lambda)
+                            self.lambda_list.append(my_lambda)
                             eigenvalue_1_list.append(eigenvalue_1)
                             eigenvalue_2_list.append(eigenvalue_2)
                             eigenvalue_3_list.append(eigenvalue_3)
                             anisotrpy_intensity_list.append(anisotrpy_intensity)
-                    
+
                     plt.figure(figsize=(8, 6))
                     plt.plot(lambda_list, eigenvalue_1_list, label = '$F$1', marker='o')
                     plt.plot(lambda_list, eigenvalue_2_list, label = '$F$2', marker='o')
                     plt.plot(lambda_list, eigenvalue_3_list, label = '$F$3', marker='o')
 
                     plt.axhline(y=1/3, color='blue', linestyle='--')
-                    
+
+
                     plt.xlabel('$\lambda$')
+
                     plt.ylabel('Eigenvalue')
                     plt.title(f'Measured eigenvalues')
                     plt.legend()
                     plt.grid('both')
-                    
+
                     pdf.savefig()
                     plt.close()
 
                     plt.figure(figsize=(8, 6))
                     plt.plot(lambda_list, anisotrpy_intensity_list, marker='o')
-                    
+
                     plt.xlabel('$\lambda$')
                     plt.ylabel('Anisotrpy intensity')
                     plt.title(f'Measured anisotrpy intensity')
+
                     #plt.legend()
                     plt.grid('both')
-                    
+
+                    pdf.savefig()
+                    plt.close()
+
+                elif file_name == "packing_properties_conductivity.txt":
+                    conductivity_tensor_list = []
+
+                    with open(file_name, 'r') as file:
+                        for line in file:
+                            conductivity_tensor = float(line.split()[1])
+                            conductivity_tensor_list.append(conductivity_tensor)
+
+                    plt.figure(figsize=(8, 6))
+                    plt.plot(self.lambda_list, conductivity_tensor_list, label = '$F$1', marker='o')
+
+                    plt.xlabel('$\lambda$')
+
+                    plt.ylabel('K')
+                    plt.title(f'Measured mean conductivity')
+                    #plt.legend()
+                    plt.grid('both')
+
                     pdf.savefig()
                     plt.close()
 
