a2ad108c7fdee596bb400b7fa1fb6111cf7a2944
Chengshun Shang <chengshun.shang1996@gmail.com>
2025-06-05 11:41:02 -0700

    minor

--- src/utilities/radius_expansion_with_servo_control_method_run.py
+++ src/utilities/radius_expansion_with_servo_control_method_run.py
@@ -216,14 +216,16 @@ class DEMAnalysisStageWithFlush(DEMAnalysisStage):
 
             stress_tensor = self.MeasureSphereForGettingGlobalStressTensor()
             mean_stress = (stress_tensor[0][0] + stress_tensor[1][1] + stress_tensor[2][2])/3
-            with open("stress_tensor_0.txt", 'a') as file:
-                    file.write(str(self.time) + ' ' + str(mean_stress) + ' ' + str(self.final_packing_density) + ' ' \
-                               + str(stress_tensor[0][0]) + ' ' + str(stress_tensor[1][1]) + ' ' + str(stress_tensor[2][2])+'\n')
+            
+            if self.is_start_servo_control:
+                with open("stress_tensor_0.txt", 'a') as file:
+                        file.write(str(self.time) + ' ' + str(mean_stress) + ' ' + str(self.final_packing_density) + ' ' \
+                                + str(stress_tensor[0][0]) + ' ' + str(stress_tensor[1][1]) + ' ' + str(stress_tensor[2][2])+'\n')
 
-            #TODO: this should be optional, not always output
-            granular_temperature, max_granular_temperature = self.GetGranularTemperature()
-            with open("granular_temperature_0.txt", 'a') as file:
-                    file.write(str(self.time) + ' ' + str(granular_temperature) + ' ' + str(max_granular_temperature) + '\n')
+                #TODO: this should be optional, not always output
+                granular_temperature, max_granular_temperature = self.GetGranularTemperature()
+                with open("granular_temperature_0.txt", 'a') as file:
+                        file.write(str(self.time) + ' ' + str(granular_temperature) + ' ' + str(max_granular_temperature) + '\n')
 
             self.measured_stress_list.append(mean_stress)
 
