8bba8887af038ebc28b88f36c2dc9c624c025c74
Chengshun Shang <chengshun.shang1996@gmail.com>
2026-03-24 17:06:13 +0100

    add info

--- src/utilities/improved_radius_expansion_with_servo_control_method_run.py
+++ src/utilities/improved_radius_expansion_with_servo_control_method_run.py
@@ -260,14 +260,31 @@ class DEMAnalysisStageWithFlush(DEMAnalysisStage):
             stress_tensor = self.MeasureSphereForGettingGlobalStressTensor()
             mean_stress = (stress_tensor[0][0] + stress_tensor[1][1] + stress_tensor[2][2])/3
 
+            mcn = self.MeasureGlobalMeanCoordinationNumber()
+            measured_conductivity, measured_conductivity_trace = self.MeasureGlobalConductivityTensor()
+
             if self.is_start_servo_control:
                 with open("stress_tensor_1.txt", 'a') as file:
                     file.write(str(self.time) + ' ' + str(mean_stress) + ' ' + str(self.final_packing_density) + ' ' \
-                                + str(stress_tensor[0][0]) + ' ' + str(stress_tensor[1][1]) + ' ' + str(stress_tensor[2][2])+'\n')
+                                + str(stress_tensor[0][0]) + ' ' + str(stress_tensor[0][1]) + ' ' + str(stress_tensor[0][2])+ ' ' \
+                                + str(stress_tensor[1][0]) + ' ' + str(stress_tensor[1][1]) + ' ' + str(stress_tensor[1][2])+ ' ' \
+                                + str(stress_tensor[2][0]) + ' ' + str(stress_tensor[2][1]) + ' ' + str(stress_tensor[2][2])+ ' ' \
+                                + str(mcn) + ' ' \
+                                + str(measured_conductivity[0][0]) + ' ' + str(measured_conductivity[0][1]) + ' ' + str(measured_conductivity[0][2]) + ' ' \
+                                + str(measured_conductivity[1][0]) + ' ' + str(measured_conductivity[1][1]) + ' ' + str(measured_conductivity[1][2]) + ' ' \
+                                + str(measured_conductivity[2][0]) + ' ' + str(measured_conductivity[2][1]) + ' ' + str(measured_conductivity[2][2]) + ' ' \
+                                + str(measured_conductivity_trace)+ '\n')
             else:
                 with open("stress_tensor_0.txt", 'a') as file:
                     file.write(str(self.time) + ' ' + str(mean_stress) + ' ' + str(self.final_packing_density) + ' ' \
-                                + str(stress_tensor[0][0]) + ' ' + str(stress_tensor[1][1]) + ' ' + str(stress_tensor[2][2])+'\n')
+                                + str(stress_tensor[0][0]) + ' ' + str(stress_tensor[0][1]) + ' ' + str(stress_tensor[0][2])+ ' ' \
+                                + str(stress_tensor[1][0]) + ' ' + str(stress_tensor[1][1]) + ' ' + str(stress_tensor[1][2])+ ' ' \
+                                + str(stress_tensor[2][0]) + ' ' + str(stress_tensor[2][1]) + ' ' + str(stress_tensor[2][2])+ ' ' \
+                                + str(mcn) + ' ' \
+                                + str(measured_conductivity[0][0]) + ' ' + str(measured_conductivity[0][1]) + ' ' + str(measured_conductivity[0][2]) + ' ' \
+                                + str(measured_conductivity[1][0]) + ' ' + str(measured_conductivity[1][1]) + ' ' + str(measured_conductivity[1][2]) + ' ' \
+                                + str(measured_conductivity[2][0]) + ' ' + str(measured_conductivity[2][1]) + ' ' + str(measured_conductivity[2][2]) + ' ' \
+                                + str(measured_conductivity_trace)+ '\n')
 
                 #TODO: this should be optional, not always output
                 '''
